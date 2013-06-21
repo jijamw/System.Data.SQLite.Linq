464a2dee8f72fcbaffe4e2947e8fd26bcb17bc35
Brice Lambson <bricelam@live.com>
2013-06-20 17:33:42 -0700

    Scoping repository to just the System.Data.SQLite.Linq project.

--- Setup/InitializeSetup.pas
+++
@@ -1,12 +0,0 @@
-{
-  InitializeSetup.iss --
-
-  Written by Joe Mistachkin.
-  Released to the public domain, use at your own risk!
-}
-
-function InitializeSetup(): Boolean;
-begin
-  Result := NetFxInitializeSetup();
-end;
-
